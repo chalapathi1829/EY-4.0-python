0b88d990ee8e27d165557d9286a92b75ba6e33de
chalapathi1829 <chalapathinaidunallani@gmail.com>
2025-09-27 11:26:17 +0530

    variables

--- app.py
+++ app.py
@@ -1,2 +1,15 @@
 #print hello world
-print("Hello world")
+# print("Hello world")
+
+# variables
+a=20
+b=30
+#assiging 
+c,d=a,b
+print(a,b,c,d)
+a=b=10
+c=d=a
+print(a,b,c,d)
+#assigning multiple values to multiple variables in one line
+x,y,z=10,20,30
+print("x:",x,"y:",y,"z:",z)
