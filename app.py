d7aa1de081e0dffabc228c0e8a4bf4419d8d2c77
chalapathi1829 <chalapathinaidunallani@gmail.com>
2025-09-27 11:28:04 +0530

    type printing

--- app.py
+++ app.py
@@ -1,15 +1,24 @@
 #print hello world
 # print("Hello world")
 
-# variables
-a=20
-b=30
-#assiging 
-c,d=a,b
-print(a,b,c,d)
-a=b=10
-c=d=a
-print(a,b,c,d)
-#assigning multiple values to multiple variables in one line
-x,y,z=10,20,30
-print("x:",x,"y:",y,"z:",z)
+# # variables
+# a=20
+# b=30
+# #assiging 
+# c,d=a,b
+# print(a,b,c,d)
+# a=b=10
+# c=d=a
+# print(a,b,c,d)
+# #assigning multiple values to multiple variables in one line
+# x,y,z=10,20,30
+# print("x:",x,"y:",y,"z:",z)
+
+#Data Types printing
+name="Jhon"
+age=23
+phone="9988775566"
+#type
+print(type(name),type(age),type(phone))
+
+
